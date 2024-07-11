f89d6d60fd99da141ae4642bb277d2a194dcf675
Abdul Bashit <abashit87@gmail.com>
2024-07-11 18:06:02 +0100

    added code

---
+++ script.js
@@ -0,0 +1,18 @@
+// Modify the code below to set the variables appropriately.
+
+// Get the form by ID from the forms collection.
+const form = false;
+// Get the todo pane (the 'ul' element).
+const todoPane = false;
+// Get the text input for the title.
+const titleInput = false;
+// Get the priority select element.
+const prioritySelect = false;
+// Get a list of all elements with the 'todo' class.
+const allTodos = false;
+
+console.log('form:', form);
+console.log('todoPane:', todoPane);
+console.log('titleInput:', titleInput);
+console.log('prioritySelect:', prioritySelect);
+console.log('allTodos:', allTodos);
